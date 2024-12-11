Fix simulated waveform scaling for lower sampling rates, Adjust column creation, Adjust concurrent execution

src/agbd_concurrent.py
@@ -86,10 +86,6 @@ def run_sim(fn_pair, config):
 	np.save(fn, agbd)
 	return fn
 
-def pop_all(l):
-	r, l[:] = l[:], []
-	return r
-
 if __name__ == '__main__':
 	diz_path = os.path.dirname(os.path.realpath(__file__))
 	config = OmegaConf.load(os.path.join(diz_path, 'config/sim_config.yaml'))
@@ -99,7 +95,7 @@ if __name__ == '__main__':
 
 	fn_pairs = get_fn_pairs(config['directory']['raw_data_dir'])
 
-	pair_iter = iter(fn_pairs[14:])
+	pair_iter = iter(fn_pairs)
 
 	with concurrent.futures.ProcessPoolExecutor(max_workers=config['concurrent']['workers_max']) as executor:
 		futures = {
@@ -115,9 +111,11 @@ if __name__ == '__main__':
 
 			for fut in done:
 				task = futures.pop(fut)
-				print(f'Completed km {fut.result()}')
+				try:
+					print(f'Completed km {fut.result()}')
+				except Exception as e:
+					print(f'Completed {task} w/ exception {e}')
 			
 			for km_fn in itertools.islice(pair_iter, len(done)):
 				fut = executor.submit(run_sim, km_fn, config)
 				futures[fut] = km_fn
-				print(futures)

src/hhdc_gen.py
@@ -15,31 +15,42 @@ import pc_block
 
 def get_hhdc_centers(block, config):
 	square_size = config['hhdc_config']['square_size']
+	
+	# Generate meshgrid for pulse centers, origin is offset by half of square size
 	x_dim = np.arange(block.bounds.left+(square_size/2), block.bounds.right, step=square_size)
 	y_dim = np.arange(block.bounds.bottom+(square_size/2), block.bounds.top, step=square_size)
-
 	x_coords, y_coords = np.meshgrid(x_dim, y_dim)
 
+	# Convert meshgrid to a list of centers
 	centers = np.dstack((x_coords, y_coords))
 	
 	return centers
 
 def prep_waveform(waveform, z_min, block_z_min, config):
+	# Convert waveform to probability density, crop zeros from beginning/end
+	# Invert waveform to have lower height bins at lower index
 	col_wf = waveform.nfw / np.sum(waveform.nfw)
-	col_wf = col_wf[np.min(np.argwhere(col_wf != 0)):np.max(np.argwhere(col_wf != 0))+1]
+	col_wf = col_wf[np.min(np.argwhere(col_wf != 0)):np.max(np.argwhere(col_wf != 0))+1][::-1]
 
+	# Calculate height offset for waveform
 	shift = int((z_min - block_z_min) / config['sim_config']['gedi_config']['resolution'])
-	shift = 0
-	
+
 	column = np.zeros((config['hhdc_config']['height_bins']), dtype=np.float32)
 
-	try:
-		column[shift:shift+len(col_wf)] = col_wf[::-1]
-	except Exception as e:
-		print(f'Error | {e}')
+	#print(f'BEFORE {shift + len(col_wf)}')
 
+	# Crop waveform to fit HHDC height limit (not ideal - dirty method for now)
+	while shift + len(col_wf) >= config['hhdc_config']['height_bins']:
+		col_wf = col_wf[:-1]
+		if len(col_wf) == 0:
+			break
+	
+	#print(f'AFTER {shift + len(col_wf)}')
 
-	return column
+	column[shift:shift + len(col_wf)] = col_wf
+
+	# Return inverted column, allows for simply transposing a slice to output DEM
+	return column[::-1]
 	
 def disp_dem(hhdc_slice, bounds, fn, indx, z_min, config):
 	bins = config['hhdc_config']['height_bins']
@@ -47,7 +58,7 @@ def disp_dem(hhdc_slice, bounds, fn, indx, z_min, config):
 
 	fig, ax = plt.subplots()
 
-	ax.imshow(hhdc_slice.T[::-1], aspect=5, interpolation='none', cmap=matplotlib.colormaps['Greens'], extent=[bounds.left,bounds.right,0,bins*resolution])
+	ax.imshow(hhdc_slice.T, aspect=5, interpolation='none', cmap=matplotlib.colormaps['Greens'], extent=[bounds.left,bounds.right,0,bins*resolution])
 	ax.set_title(f'Digital Elevation Model for Across Track Slice {indx}')
 	ax.set_ylabel(f'Height from Reference Ground: {np.round(z_min,1)} [m]')
 	ax.set_xlabel(f'Along Track Coordinate [m]')
@@ -56,7 +67,7 @@ def disp_dem(hhdc_slice, bounds, fn, indx, z_min, config):
 	plt.close()
 
 def get_fn_pairs(data_dir):
-	cam_files_dir = os.path.join(data_dir, 'camera/')
+	cam_files_dir = os.path.join(data_dir, 'test/')
 	laz_files_dir = os.path.join(data_dir, 'lidar/')
 
 	cam_files = [f for f in os.listdir(cam_files_dir) if os.path.isfile(os.path.join(cam_files_dir, f))]
@@ -75,11 +86,13 @@ def get_fn_pairs(data_dir):
 
 	return full_kms_fn
 
-def run_sim(fn_pair, output, config):
+def run_sim(fn_pair, config):
 	img_fn = fn_pair[0]
 	pc_fn = fn_pair[1]
 
 	bl = '_'.join(img_fn.split('_')[-3:-1])
+
+	print(f'Running {bl} ...')
 
 	block = pc_block.Block(img_fn, pc_fn)
 	centers = get_hhdc_centers(block, config)
@@ -89,61 +102,72 @@ def run_sim(fn_pair, output, config):
 
 	pulses = pc_block.Pulses(block, centers, config)
 
+	# Minimum Z-value for entire block, used to calculate vertical offset for each waveform
 	block_z_min = np.min(block.photons[:,2])
 
 	for indx in range(np.shape(centers)[0]):
+		print(f'Computing center {indx}/{np.shape(centers)[0]} ...')
 		if len(pulses.ret_photons[indx].photons) == 0:
 			continue
 
 		sim_wf = simulation.Waveform(block.photons, pulses.ret_photons[indx], block.ground, pulses.ret_ground[indx], config)
+
 		z_min = np.min(block.photons[pulses.ret_photons[indx].photons][:,2])
 
 		column = prep_waveform(sim_wf, z_min, block_z_min, config)
-		
+
 		ix = indx % np.shape(hhdc)[0]
 		iy = int(indx / np.shape(hhdc)[1])
 
 		hhdc[iy, ix, :] = column
 
-	fn = f'{output}SJER_{bl}_cc_hhdc_20x20_gridded.npy'
+	fn = f"{config['directory']['output_hhdc']}SJER_{bl}_hhdc_20x20_gridded.npy"
 	np.save(fn, hhdc)
 	return fn
 
 if __name__ == '__main__':
-	target = 'data/raw/SJER/2023-04/'
-	bl = '252000_4109000'
-	output = 'output_hhdc_cc/'
-
 	# True if concurrency enabled
 	cc = False
-
-	fn_pairs = get_fn_pairs(target)
-	print(fn_pairs)
 
 	diz_path = os.path.dirname(os.path.realpath(__file__))
 	config = OmegaConf.load(f'{diz_path}/config/sim_config.yaml')
 
+	if not os.path.exists(config['directory']['output_hhdc']):
+		os.makedirs(config['directory']['output_hhdc'])
+
+	fn_pairs = get_fn_pairs(config['directory']['raw_data_dir'])
+
+	print(fn_pairs)
+
+	count = 0
 	if cc:
-		max_workers = 5
-		fn_pairs_iter = iter(fn_pairs)
-		with tqdm(total=len(fn_pairs)) as pbar:
+		fn_pairs_iter = iter(fn_pairs[135:])
 
-			with concurrent.futures.ProcessPoolExecutor(max_workers=max_workers) as executor:
-				futures = {executor.submit(run_sim, km_fn, output, config) for km_fn in itertools.islice(fn_pairs_iter, max_workers)}
-				print(len(futures))
+		with concurrent.futures.ProcessPoolExecutor(max_workers=config['concurrent']['workers_max']) as executor:
+			futures = {
+				executor.submit(run_sim, km_fn, config): km_fn for km_fn in itertools.islice(fn_pairs_iter, config['concurrent']['workers_max'])
+			}
 
-				while len(futures) > 0:
-					done, futures = concurrent.futures.wait(futures, return_when=concurrent.futures.FIRST_COMPLETED)
+			while futures:
+				done, _ = concurrent.futures.wait(
+					futures, return_when=concurrent.futures.FIRST_COMPLETED	
+				)
 
-					for fut in done:
-						print(f'\n{len(futures)}\nTask {fut} completed w/ result: {fut.result()}')
-						
-						pbar.update(1)
+				for fut in done:
+					task = futures.pop(fut)
+					try:
+						print(f'Completed km {fut.result()}')
+					except Exception as e:
+						print(f'Completed {task} w/ exception {e}')
+				
+				for km_fn in itertools.islice(fn_pairs_iter, len(done)):
+					fut = executor.submit(run_sim, km_fn, config)
+					futures[fut] = km_fn
+					count += 1
+					print(f'Completed: {count}')
 
-
-					for km_fn in itertools.islice(fn_pairs_iter, len(done)):
-						futures.add(executor.submit(run_sim, km_fn, output, config))
 	else:
+
 		for km_fn in fn_pairs:
-			res = run_sim(km_fn, output, config)
+			res = run_sim(km_fn, config)
 			print(f'Completed {res}')

src/simulation.py
@@ -5,15 +5,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 from omegaconf import OmegaConf
+from scipy.signal import argrelextrema
 
 import pc_block
 
 def gauss_weight(d2, sigma):
 	return np.exp(-1*d2/(2*np.square(sigma)))/(np.sqrt(2*np.pi)*sigma)
 
-def get_bins(photons):
-		zmin = int(np.min(photons[:,2])-5)
-		zmax = int(np.max(photons[:,2])+5)
+def get_bins(photons, ground):
+		#print(photons)
+		#print(ground)
+		
+		zmin = np.min(int(np.min(photons[:,2])-5))
+		zmax = np.max(int(np.max(photons[:,2])+5))
+
+		if len(ground) != 0:
+			zmin = np.min([int(np.min(ground[:,2])-5), zmin])
+			zmax = np.max([int(np.max(ground[:,2])+5), zmax])
+
+		#print((gmin, gmax))
+
+		#tmp = (np.min([gmin, zmin]), np.max([gmax, zmax]))
+		#print(tmp)
+
 		return (zmin, zmax)
 
 def get_centers(block, config):
@@ -24,7 +38,20 @@ def get_centers(block, config):
 			centers.append([block.pos_matrix[indx, indy, 0], block.pos_matrix[indx, indy, 1]])
 
 	return centers
+# // TEMP
+def get_hhdc_centers(block, config):
+	square_size = config['hhdc_config']['square_size']
+	
+	# Generate meshgrid for pulse centers, origin is offset by half of square size
+	x_dim = np.arange(block.bounds.left+(square_size/2), block.bounds.right, step=square_size)
+	y_dim = np.arange(block.bounds.bottom+(square_size/2), block.bounds.top, step=square_size)
+	x_coords, y_coords = np.meshgrid(x_dim, y_dim)
 
+	# Convert meshgrid to a list of centers
+	centers = np.dstack((x_coords, y_coords))
+	
+	return centers
+# // TEMP
 class Waveform():
 	def __init__(self, photons, collected, gnd_phot, ground, config):
 		self.nfw, self.gnd = self.get_waveforms(photons, collected, gnd_phot, ground, config)
@@ -55,18 +82,29 @@ class Waveform():
 		if len(sel_photons) == 0:
 			return None, None
 
-		bounds = get_bins(sel_photons)
+		bounds = get_bins(sel_photons, sel_ground)
 		nfw = self.simulate(sel_photons, collected.center, bounds, config)
-		gnd = self.simulate(sel_ground, ground.center, bounds, config)
+
+		gnd = np.zeros((int((bounds[1]-bounds[0])/config['sim_config']['gedi_config']['resolution'])))
+		idx = argrelextrema(nfw, np.greater)[-1][-1]
+		gnd[idx] = nfw[idx]
+
+		if len(sel_ground) != 0:
+			gnd = self.simulate(sel_ground, ground.center, bounds, config)
+
 		return nfw, gnd
 
 	def simulate(self, photons, center, bounds, config):
 		arr = np.zeros((int((bounds[1]-bounds[0])/config['sim_config']['gedi_config']['resolution'])))
 
+		#print(f'B1 {bounds[1]} | B2 {bounds[0]} | ARR {len(arr)}')
+
 		weighted_intensity = [[p[2], p[3]*gauss_weight(np.square(p[0]-center[0])+np.square(p[1]-center[1]),sigma=0.25*config['sim_config']['gedi_config']['fwidth'])] for p in photons]
 		for pair in weighted_intensity:
+			#print(f'B1 {bounds[1]} | B2 {pair[0]}')
 			bucket = int(np.ceil((bounds[1]-pair[0])/config['sim_config']['gedi_config']['resolution']))
 			arr[bucket] += pair[1]
+			
 
 		pulse = pulse = [gauss_weight(np.square(n),sigma=config['sim_config']['gedi_config']['fwhm']/2.354) for n in np.arange(-25,25)]
 		nfw = np.convolve(pulse, arr)
@@ -80,20 +118,20 @@ def plot_waveforms(waveform):
 	ax.plot(waveform.nfw)
 	#ax.plot(waveform.gnd)
 
-	for indx in waveform.rh_idx:
-		ax.axvline(x=indx, color='r')
+	for rh in waveform.rh_idx:
+		ax.axvline(x=waveform.rh_idx[rh], color='r')
 	plt.show()
 	plt.close()
 
 if __name__ == '__main__':
 	target = 'data/raw/SJER/2023-04/'
-	bl = '251000_4106000'
+	bl = '258000_4113000'
 	output = 'output_ref/'
 
 	if not os.path.exists(output):
 		os.makedirs(output)
 
-	img_fn = f'{target}camera/2023_SJER_6_{bl}_image.tif'
+	img_fn = f'{target}camera/2021_SJER_5_{bl}_image.tif'
 	pc_fn = f'{target}lidar/NEON_D17_SJER_DP1_{bl}_classified_point_cloud_colorized.laz'
 
 	diz_path = os.path.dirname(os.path.realpath(__file__))
@@ -101,15 +139,21 @@ if __name__ == '__main__':
 	print(config)
 
 	block = pc_block.Block(img_fn, pc_fn)
-	centers = get_centers(block, config)
+	#centers = get_centers(block, config)
+	centers = get_hhdc_centers(block, config)
+	centers = np.concatenate(centers, axis=0)[2060:]
+	print(len(centers))
 	pulses = pc_block.Pulses(block, centers, config)
 
+	#print(len(pulses.pulse_centers))
 	for indx in range(len(pulses.pulse_centers)):
+		print(indx)
 		if len(pulses.ret_photons[indx].photons) == 0:
 			continue
 
 		sim_wf = Waveform(block.photons, pulses.ret_photons[indx], block.ground, pulses.ret_ground[indx], config)
+		#print(sim_wf)
 		if sim_wf.nfw is None:
 			continue
 		print(sim_wf.rh)
-		plot_waveforms(sim_wf)
+		#plot_waveforms(sim_wf)